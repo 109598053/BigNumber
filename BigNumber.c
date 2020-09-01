2136b3dc0d6990d2029ade6d278aa184e70596d2
Sandy <t109598053@ntut.org.tw>
2020-09-01 18:21:06 +0800

    加減合併計算，負數過大問題未解決

--- BigNumber.c
+++ BigNumber.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #define MAX 1000
-void add(char *arr1, char *arr2);
-void * sub(char *arr1, char *arr2);
+void count(char *arr1, char *arr2);
+void isPositive(char *arr1, char *arr2);
+//void sub(char *arr1, char *arr2);
 char * mult(char *arr1, char *arr2);
+int positive1, positive2;
 
 void main(){
 	char *arr1 = malloc(sizeof(char));
@@ -22,38 +24,14 @@ void main(){
 	printf("請輸入運算子:");
     ope = getchar();
 	
-	char positive1 = '+', positive2 = '+';
-	if(arr1[0] == '-'){
-		positive1 = '-';
-		for (i = 0; i < strlen(arr1); i++)
-	    {
-	        *(arr1 + i) = *(arr1 + i + 1);
-	    }
-	}
-	if(arr2[0] > '9' || arr2[0] < '0'){
-		positive2 = '-';
-		for (i = 0; i < strlen(arr2); i++)
-	    {
-	        *(arr2 + i) = *(arr2 + i + 1);
-	    }
-	}
-	
-	int len, len1 = strlen(arr1), len2 = strlen(arr2);
+	isPositive(arr1, arr2);
 	
 	switch(ope){
 		case '+':
-			add(arr1, arr2);
-			break;
 		case '-':
-			sub(arr1, arr2);
+			count(arr1, arr2);
 			break;
 		case '*':
-			if(len1 > len2){
-				len = len1*2;
-			}
-			else{
-				len = len2*2;
-			}
 //			ans = mult(arr1, arr2);
 			break;
 	}
@@ -75,18 +53,19 @@ void main(){
 	free(arr2);
 }
 
-void add(char *arr1, char *arr2)
+void count(char *arr1, char *arr2)
 {
-	int *ans = malloc( MAX *sizeof(int));
+	int *ans = calloc( MAX,  sizeof(int));
 	int len, len1 = strlen(arr1), len2 = strlen(arr2), i=0, j = len1 - 1, k = len2 - 1;
+	printf("p1:%d p2:%d\n", positive1, positive2);
 	while(j >= 0 && k >= 0){
-		ans[i++]=(arr1[j--] - '0') + (arr2[k--] - '0');
+		ans[i++]=(arr1[j--] - '0') * positive1 + (arr2[k--] - '0') * positive2;
 	}
 	while(j >= 0){
-		ans[i++] = arr1[j--] - '0';
+		ans[i++] = (arr1[j--] - '0') * positive1;
 	}
 	while(k >= 0){
-		ans[i++] = arr2[k--] - '0';
+		ans[i++] = (arr2[k--] - '0') * positive2;
 	}
 	if(len1 > len2){
 		len = len1+1;
@@ -99,22 +78,44 @@ void add(char *arr1, char *arr2)
 			ans[i+1]+=ans[i]/10;
 			ans[i]=ans[i]%10;
 		}
+		else if(ans[i]<0 && ans[i+1]){ //&& ans[10]>-10???
+			ans[i+1] -= 1;
+			ans[i] = 10 - ans[i];
+		}
 	}
+	int result = 0;
 	for(i = len-1; i >= 0; i--){
-		int a = ans[i];
-		if(i==len-1 && a==0){
-			continue;
-		}
-		else{
-	    	printf("%d", a);
-		}
+		result += (ans[i] * pow(10, i));
 	}
+	printf("result:%d\n", result);
 }
-void * sub(char *arr1, char *arr2)
+void isPositive(char *arr1, char *arr2)
 {
+	int i;
+	positive1 = 1, positive2 = 1;
+	if(arr1[0] == '-'){
+		positive1 = -1;
+		for (i = 0; i < strlen(arr1); i++)
+	    {
+	        *(arr1 + i) = *(arr1 + i + 1);
+	    }
+	}
+	if(arr2[0] == '-'){
+		positive2 = -1;
+		for (i = 0; i < strlen(arr2); i++)
+	    {
+	        *(arr2 + i) = *(arr2 + i + 1);
+	    }
+	}
 }
 char * mult(char *arr1, char *arr2)
 {
-    /* Implement function `hello()` here. */
+	int len, len1 = strlen(arr1), len2 = strlen(arr2), i=0, j = len1 - 1, k = len2 - 1;
+	if(len1 > len2){
+		len = len1*2;
+	}
+	else{
+		len = len2*2;
+	}
     return arr1;
 }
