b52d3f76bb906d1f865c9c6c79c65bd2176374d7
Sandy <t109598053@ntut.org.tw>
2020-09-01 15:29:45 +0800

    暫存

--- BigNumber.c
+++ BigNumber.c
@@ -1,29 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+void add(char *arr1, char *arr2);
+char * sub(char *arr1, char *arr2);
+char * mult(char *arr1, char *arr2);
 
 void main(){
-//	int size = 200;
-	char *arr1;
-	char *arr2;
-	arr1 = malloc(sizeof(char));
-	arr2 = malloc(sizeof(char));
-	char ch1;
-	char ch2;
-
-	int i = 0;
-	printf("Input1:");
-	while((ch1 = getchar()) != '\n'){
-		*(arr1+i++) = ch1;
-	}
-	printf("Input2:");
-	i = 0;
-	while((ch2 = getchar()) != '\n'){
-		*(arr2+i++) = ch2;
-	}
+	char *arr1 = malloc(sizeof(char));
+	char *arr2 = malloc(sizeof(char));
+	char ch1, ch2, ope;
+	int i, *arr = malloc(sizeof(int));
 	
-//	add();
-//	int size = strlen(arr);
+	printf("Input1:");
+    scanf("%s", arr1);
+	
+	printf("Input2:");
+    scanf("%s", arr2);
+    
+	int len, len1 = strlen(arr1), len2 = strlen(arr2);
+    
+    fflush(stdin);
+	printf("請輸入運算子:");
+    ope = getchar();
+	
+	switch(ope){
+		case '+':
+			add(arr1, arr2);
+			break;
+		case '-':
+			if(len1 > len2){
+				len = len1;
+			}
+			else{
+				len = len2;
+			}
+//			ans = sub(arr1, arr2);
+			break;
+		case '*':
+			if(len1 > len2){
+				len = len1*2;
+			}
+			else{
+				len = len2*2;
+			}
+//			ans = mult(arr1, arr2);
+			break;
+	}
+
+        printf("\n");
 	for (i = 0; i < strlen(arr1); i++)
     {
         printf("%c", *(arr1 + i));
@@ -33,7 +57,65 @@ void main(){
     {
         printf("%c", *(arr2 + i));
     }
+	printf("\noperator:%c\n", ope);
+    
     
 	free(arr1);
 	free(arr2);
+}
+
+void add(char *arr1, char *arr2)
+{
+	int *ans = malloc(sizeof(int));
+	int i=0, j = strlen(arr1) - 1, k = strlen(arr2) - 1;
+	while(j >= 0 && k >= 0){
+		printf("\narr1: \n", (arr1[j] - '0') + (arr1[j] - '0'));
+		ans[i++]=arr1[j--] + arr2[k--];
+	}
+	if(j >= 0){
+		while(k >= 0){
+			ans[i++] = arr1[j--] - '0';
+		}
+	}
+	else if(k >= 0){
+		while(k >= 0){
+			ans[i++] = arr2[k--] - '0';
+		}
+	}
+	printf("\narr1: \n");
+	for (i = strlen(arr1)-1; i >= 0; i--)
+    {
+        printf("%c ", *(arr1 + i));
+    }
+	printf("\nans: \n");
+	for (i = strlen(arr1)-1; i >= 0; i--)
+    {
+        printf("%d ", *(ans + i));
+    }
+	printf("\nans: %d\n", ans);
+	for (i = 0; i < strlen(arr1); i++)
+    {
+        printf(" %d", ans[i]);
+    }
+	for(i = 0; i < strlen(arr1); i++){
+		if(ans[i]>=10) {
+			ans[i+1]+=ans[i]/10;
+			ans[i]=ans[i]%10;
+		}
+	}
+	printf("\nans:");
+	for (i = 0; i < strlen(arr1); i++)
+    {
+        printf("%c", *(ans + i));
+    }
+	printf("add:%s\n", ans);
+}
+char * sub(char *arr1, char *arr2)
+{
+    return arr1;
+}
+char * mult(char *arr1, char *arr2)
+{
+    /* Implement function `hello()` here. */
+    return arr1;
 }
